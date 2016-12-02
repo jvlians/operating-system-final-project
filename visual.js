b8c6ad5df52b1bfa1e655f700ac6fac3165dc04d
Julian Slaughter <julians@maxxpotential.com>
2016-12-01 21:14:02 -0500

    Resolved the mystery of the disappearing jobs

--- visual.js
+++ visual.js
@@ -1,10 +1,23 @@
 function updateVisuals() {
+
+	var tQueue	= sched.getTerminatedQueue();
+	var tVis	= document.getElementById("terminatedQueue");
+	for (var i = 0; i < tQueue.length; i++) {
+		removeElement("#readyQueue #j"+tQueue[i].getId());
+		removeElement("#waitingQueue #j"+tQueue[i].getId());
+		if (tVis.textContent.indexOf("["+tQueue[i].getId()+"]") > -1) {
+			updateContents("#terminatedQueue #j"+tQueue[i].getId()+" #data",tQueue[i].getElementWithoutContainer());
+		} else {
+			addContents("#terminatedQueue",tQueue[i].getElementWithContainer());
+		}
+	}
+
 	var rQueue	= sched.getReadyQueue();
 	var rVis	= document.getElementById("readyQueue");
 	for (var i = 0; i < rQueue.length; i++) {
 		removeElement("#waitingQueue #j"+rQueue[i].getId());
-		if (rVis.textContent.indexOf(rQueue[i].getId()) > -1) {
-			updateContents("#readyQueue #j"+i+" #data",rQueue[i].getElementWithoutContainer());
+		if (rVis.textContent.indexOf("["+rQueue[i].getId()+"]") > -1) {
+			updateContents("#readyQueue #j"+rQueue[i].getId()+" #data",rQueue[i].getElementWithoutContainer());
 		} else {
 			addContents("#readyQueue",rQueue[i].getElementWithContainer());
 		}
@@ -14,23 +27,10 @@ function updateVisuals() {
 	var wQueue	= sched.getWaitingQueue();
 	var wVis	= document.getElementById("waitingQueue");
 	for (var i = 0; i < wQueue.length; i++) {
-		removeElement("#readyQueue #j"+wQueue[i].getId());
-		if (wVis.textContent.indexOf(wQueue[i].getId()) > -1) {
-			updateContents("#waitingQueue #j"+i+" #data",wQueue[i].getElementWithoutContainer());
+		if (wVis.textContent.indexOf("["+wQueue[i].getId()+"]") > -1) {
+			updateContents("#waitingQueue #j"+wQueue[i].getId()+" #data",wQueue[i].getElementWithoutContainer());
 		} else {
 			addContents("#waitingQueue",wQueue[i].getElementWithContainer());
-		}
-	}
-
-	var tQueue	= sched.getTerminatedQueue();
-	var tVis	= document.getElementById("terminatedQueue");
-	for (var i = 0; i < tQueue.length; i++) {
-		removeElement("#readyQueue #j"+tQueue[i].getId());
-		removeElement("#waitingQueue #j"+tQueue[i].getId());
-		if (tVis.textContent.indexOf(tQueue[i].getId()) > -1) {
-			updateContents("#terminatedQueue #j"+i+" #data",tQueue[i].getElementWithoutContainer());
-		} else {
-			addContents("#terminatedQueue",tQueue[i].getElementWithContainer());
 		}
 	}
 }
@@ -51,7 +51,6 @@ function removeElement(selector) {
 	if (rem) {
 		rem.remove();
 	}
-	document.querySelector(selector).remove();
 }
 
 
